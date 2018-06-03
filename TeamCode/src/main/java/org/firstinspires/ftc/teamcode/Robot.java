7e1b07d2ed1ac693a308aa4b5326a04f0737e876
karan <karkeerthy2445@gmail.com>
2018-06-03 19:32:36 -0400

    hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -7,14 +7,18 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "teleOp", group = "TeleOp")
 public class Robot extends LinearOpMode {
 
-    DcMotor left;
-    DcMotor right;
+    DcMotor L1;
+    DcMotor L2;
+    DcMotor R1;
+    DcMotor R2;
 
     @Override
     public void runOpMode()
     {
-        left = hardwareMap.dcMotor.get("left motor");
-        right = hardwareMap.dcMotor.get("right motor");
+        L1 = hardwareMap.dcMotor.get("L1");
+        L2 = hardwareMap.dcMotor.get("L2");
+        R1 = hardwareMap.dcMotor.get("R1");
+        R2 = hardwareMap.dcMotor.get("R2");
         waitForStart();
 
         while (opModeIsActive())
@@ -28,8 +32,10 @@ public class Robot extends LinearOpMode {
             //Tank Drive
             double leftpower = -gamepad1.left_stick_y;
             double rightpower = -gamepad1.right_stick_y;
-            left.setPower(Range.clip((leftpower), -1, 1));
-            right.setPower(Range.clip((rightpower), -1, 1));
+            L1.setPower(Range.clip((leftpower), -1, 1));
+            L2.setPower(Range.clip((leftpower), -1, 1));
+            R1.setPower(Range.clip((rightpower), -1, 1));
+            R2.setPower(Range.clip((rightpower), -1, 1));
             idle();
 
         }
